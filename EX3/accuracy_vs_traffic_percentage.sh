4b16c29855a495429b55dabce48661d85fd32c32
Kyungtae Kim <kyungt4658@gmail.com>
2025-08-15 13:09:06 -0500

    Update accuracy_vs_traffic_percentage.sh

--- EX3/accuracy_vs_traffic_percentage.sh
+++ EX3/accuracy_vs_traffic_percentage.sh
@@ -3,3 +3,4 @@
 python main.py --num_epochs 100 --batch_size 64 --file_name dataset_WTCM/USA1  --num_cls 10 --early_stage True  
 # python main.py --num_epochs 100 --batch_size 64 --file_name dataset_WTCM/USA2  --num_cls 10 --early_stage True  
 # python main.py --num_epochs 100 --batch_size 64 --file_name dataset_WTCM/South_Korea  --num_cls 10 --early_stage True 
+ 
